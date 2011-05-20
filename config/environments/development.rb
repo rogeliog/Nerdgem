998c0920ea0c65e71852eb418fea2fc335207cd9
Rogelio Guzman <chinog9@gmail.com>
2011-05-20 16:41:00 -0500

    Added simple devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,7 @@ Nerdgem::Application.configure do
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Nerdgem::Application.routes.draw do
+  devise_for :users
+
+  root :to => "users#index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
