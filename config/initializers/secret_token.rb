02713f93bee8b9c5a9f9e0ab1bff90a1a8cc592d
Rogelio Guzman <chinog9@gmail.com>
2011-05-20 16:29:00 -0500

    Brand new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nerdgem::Application.config.secret_token = 'ccd4ea8d72374655fcf854a248f54b7f140bad10b97746de56383794527f3d4bb3357640e426d18a3e7b639b090354e92c3856e3c985bbd9b5a3a9986ff6d4e5'
