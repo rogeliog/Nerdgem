944b7b5157095a154a4cd65c83ba53039a5f9f6a
Rogelio Guzman <chinog9@gmail.com>
2011-11-20 13:11:14 -0600

    Integrated Bootstrap and first iteration on the new design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,5 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
   end
 
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def notice_trans(msg)
+      msg.to_s == "alert" || msg.to_s == "error" ? "alert-message error" : "alert-message success"
+  end
 end
