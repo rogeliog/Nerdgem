1c1665b367e320e78dff8011776d56d848c28d91
Rogelio Guzman <chinog9@gmail.com>
2011-12-01 12:55:44 -0600

    Trying to fix the uid cast crash

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -5,12 +5,12 @@ class AuthenticationsController < ApplicationController
 
   def create
     omniauth = request.env["omniauth.auth"]
-    authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'])
+    authentication = Authentication.find_by_provider_and_uid(omniauth['provider'], omniauth['uid'].to_s)
     if authentication
       flash[:notice] = "Signed in successfully."
       sign_in_and_redirect(:user, authentication.user)
     elsif current_user
-      current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'])
+      current_user.authentications.create!(:provider => omniauth['provider'], :uid => omniauth['uid'].to_s)
       flash[:notice] = "Authentication successful."
       redirect_to authentications_url
     else
