67751339e74a43c545b70c99e9b439df2d6f7f3c
Rogelio Guzman <chinog9@gmail.com>
2012-01-16 12:36:01 +0100

    Addded simple user profile

---
+++ spec/requests/Users/user_profile_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'User Profile' do
+  it 'Shows the user info' do
+    sign_in
+    click_link USER['name']
+    page.should have_content "Your profile"
+    within '#user' do
+      page.should have_content USER['email']
+      page.should have_content USER['name']
+    end
+  end
+
+  it 'Show the users tutuorials' do
+    user = sign_in
+    click_link USER['name']
+    Factory(:tutorial, :user => user)
+    within '#user' do
+      user.tutorials do |tutorial|
+        page.should have_content tutorial.title
+      end
+    end
+  end
+
+end
