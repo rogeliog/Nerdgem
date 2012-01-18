72af5ac0447a6192e673b01d24f0c714ee446763
Rogelio Guzman <chinog9@gmail.com>
2012-01-18 18:52:07 +0100

    Fixed PG error on related tutorials

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -41,7 +41,11 @@ class Tutorial < ActiveRecord::Base
   end
 
   def related_tutorials
-    ([self] + Tutorial.joins(:ruby_gems).where("ruby_gems.id IN (#{ruby_gems_ids.join(',')})")).uniq if ruby_gems_ids.present?
+    if ruby_gems_ids.present?
+      ([self] + Tutorial.joins(:ruby_gems).where("ruby_gems.id IN (#{ruby_gems_ids.join(',')})")).uniq 
+    else
+      [self]
+    end
   end
 
   def ruby_gems_ids
