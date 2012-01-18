bc527ffe3143580fba4cd7f4bf5d17a21b72088f
Rogelio Guzman <chinog9@gmail.com>
2012-01-18 18:58:15 +0100

    Added ruby_gems#index back again

--- app/controllers/ruby_gems_controller.rb
+++ app/controllers/ruby_gems_controller.rb
@@ -1,4 +1,12 @@
 class RubyGemsController < ApplicationController
+  def index
+    @ruby_gems = RubyGem.where("name #{LIKE} ?", "#{params[:q]}%").order("name")
+    respond_to do |format|
+      format.html
+      format.json { render :json => @ruby_gems.map(&:attributes) }
+    end
+  end
+
   def show
     @ruby_gem  = RubyGem.find (params[:id])
     @ruby_gem.get_info
