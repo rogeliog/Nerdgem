05a29b2273259ee99824e74e3e82d2bc3970518d
Rogelio Guzman <chinog9@gmail.com>
2012-01-23 22:08:36 +0100

    Added ensure domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :ensure_domain
+
+  APP_DOMAIN = 'www.nerdgem.com'
+
+  def ensure_domain
+    if Rails.env.production? && request.env['HTTP_HOST'] != APP_DOMAIN
+      # HTTP 301 is a "permanent" redirect
+      redirect_to "http://#{APP_DOMAIN}", :status => 301
+    end
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     flash[:alert] = 'You are not authorized to access this page.'
-      redirect_to root_path
+    redirect_to root_path
   end
 
 
